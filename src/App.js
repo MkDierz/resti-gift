614098d135840bebc1013682b9ccee90fdb00937
MkDierz <msyibbran24@gmail.com>
2022-04-01 13:41:28 +0700

    add talilwind

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container bg-slate-300 h-screen'>
+        <div className='flex'>
+          <span className='p-4 font-bold text-center w-full'>
+            HERE
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
